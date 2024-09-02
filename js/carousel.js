eca2c890b38955e3ce651209ab0b0048208e8cb3
krialex <56829037+krialex@users.noreply.github.com>
2024-09-02 13:03:50 +0200

    gitignore added

---
+++ js/carousel.js
@@ -0,0 +1,12 @@
+const leftArrow = document.querySelector('.fa-solid.fa-arrow-left');
+const rightArrow = document.querySelector('.fa-solid.fa-arrow-right');
+const carusellContainer = document.querySelector('.carusell');
+
+
+leftArrow.addEventListener('click', () => {
+    carusellContainer.scrollLeft -= 400;
+});
+
+rightArrow.addEventListener('click', () => {
+    carusellContainer.scrollLeft += 400;
+});
